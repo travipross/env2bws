b6fbbc23c322ac50303d42f25344d092fdc7edf7
Travis Prosser <travisprosser@gmail.com>
2025-02-23 20:53:24 -0400

    feat!:remove cli module from public API of crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 #![doc = include_str!("../README.md")]
-pub use cli::Cli;
 pub use dotenv::{DotEnvFile, EnvVar};
 pub use import_payload::{ImportPayload, Project, ProjectAssignment, Secret};
 
-pub mod cli;
 pub mod dotenv;
 pub mod import_payload;
 
